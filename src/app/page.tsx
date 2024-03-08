f047ce00533af2481a96fd7d6bda0fc2c94b03e9
FeatureKreachure <43717996+FeatureKreachure@users.noreply.github.com>
2024-03-08 22:33:09 +0200

    navbar + svgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>I'm Gonna Call THis Product Cart</h1>
+    <main>
+      <div>hello</div>
     </main>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
-
-// navbar
+import React from "react";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav>
+      <div>
+        {/* Top Section */}
+        <div className="flex justify-between p-4 bg-slate-800">
+          {/** div1 - Centered Horizontally */}
+          <div className="flex gap-4 text-white mx-auto">
+            <p>TODAY'S DEALS</p>
+            <p>EVERYDAY ESSENTIALS</p>
+          </div>
 
-export default NavBar
+          {/** div2 - Appears on the Right */}
+          <div className="flex gap-4 text-white">
+            <p className="ml-auto">MY ACCOUNT</p>
+            <p>CART</p>
+          </div>
+        </div>
+
+        {/* Bottom Section */}
+        <div className="flex justify-between items-center p-4 bg-blue-600">
+          <div className="flex flex-col">
+            <div>
+              <p className="text-white">one</p>
+              <p className="text-white">
+                day<span className="text-black">only</span>
+              </p>
+            </div>
+          </div>
+
+          <div className="text-white">
+            <p>Deals Expire In:</p>
+            <p>02:04:56</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
