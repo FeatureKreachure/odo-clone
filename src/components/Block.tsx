93305b022f00d4d95d841484cea09aba2ae54f52
FeatureKreachure <43717996+FeatureKreachure@users.noreply.github.com>
2024-03-09 10:53:43 +0200

    navbar improvement

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
 
-// the main product 'block' which has product info
-
-const Block = () => {
-  return (
-    <div>Block</div>
-  )
+interface BlockProps {
+  imageSrc: string;
+  title: string;
+  description: string;
+  price: number;
+  knockoff: number;
 }
 
-export default Block
+const Block = ({
+  imageSrc,
+  title,
+  description,
+  price,
+  knockoff,
+}: BlockProps) => {
+  return (
+    <div className="block-container">
+      <img src={imageSrc} alt="Square Image" className="square-image" />
+
+      <div className="info-container">
+        <h2 className="title">{title}</h2>
+        <p className="description">{description}</p>
+
+        <div className="price-container">
+          <p className="price">${price}</p>
+          <p className="knockoff">${knockoff}</p>
+        </div>
+
+        <button className="add-button">Add</button>
+      </div>
+    </div>
+  );
+};
+
+export default Block;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
@@ -5,32 +6,38 @@ const NavBar = () => {
     <nav>
       <div>
         {/* Top Section */}
-        <div className="flex justify-between p-4 bg-slate-800">
+        <div className="flex justify-between px-3 bg-[#272727]">
+          <h1 className="font-bold text-[#272727]">somethingelse</h1>
+
           {/** div1 - Centered Horizontally */}
-          <div className="flex gap-4 text-white mx-auto">
-            <p>TODAY'S DEALS</p>
-            <p>EVERYDAY ESSENTIALS</p>
+          <div className="flex gap-2 text-[#D4DCE2]">
+            <p className="hover:bg-[#0093d0]">TODAY'S DEALS</p>
+            <p className="hover:bg-[#0093d0]">EVERYDAY ESSENTIALS</p>
           </div>
 
           {/** div2 - Appears on the Right */}
-          <div className="flex gap-4 text-white">
-            <p className="ml-auto">MY ACCOUNT</p>
-            <p>CART</p>
+          <div className="text-white">
+            <Link href={"/register"} className="mr-2">
+              MY ACCOUNT
+            </Link>
+            <Link href={"/cart"} className="">
+              CART
+            </Link>
           </div>
         </div>
 
         {/* Bottom Section */}
-        <div className="flex justify-between items-center p-4 bg-blue-600">
-          <div className="flex flex-col">
-            <div>
-              <p className="text-white">one</p>
-              <p className="text-white">
-                day<span className="text-black">only</span>
-              </p>
-            </div>
+        <div className="flex justify-between px-3 text-white text-3xl font-bold bg-[#0093d0]">
+          {/** div1 appears left */}
+          <div>
+            <p className="mb-0.5">one</p>
+            <p className="mb-0.5">
+              day<span className="text-black">only</span>
+            </p>
           </div>
 
-          <div className="text-white">
+          {/** div2 appears right */}
+          <div className="mb-1">
             <p>Deals Expire In:</p>
             <p>02:04:56</p>
           </div>
